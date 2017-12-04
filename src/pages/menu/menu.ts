614e00c88e827dbbc4ced18498983c0f0857cd83
torekhan <torekhan93@mail.ru>
2017-12-04 21:13:53 +0000

    Alerts, Toasts and Loading

--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 import { Dish } from '../../shared/dish';
 import { DishProvider } from '../../providers/dish/dish';
 import { FavoriteProvider } from '../../providers/favorite/favorite';
@@ -23,6 +23,7 @@ export class MenuPage implements OnInit {
   constructor(public navCtrl: NavController, public navParams: NavParams,
     private dishservice: DishProvider,
     private favoriteservice: FavoriteProvider,
+    private toastCtrl: ToastController,
     @Inject('BaseURL') private BaseURL) { }
   
   ngOnInit() {
@@ -45,5 +46,9 @@ export class MenuPage implements OnInit {
   addToFavorites(dish: Dish) {
     console.log('Adding to Favorites', dish.id);
     this.favoriteservice.addFavorite(dish.id);
+    this.toastCtrl.create({
+      message: 'Dish ' + dish.id + ' added as a favorite successfully',
+      duration: 3000
+    }).present();
   }
 }
